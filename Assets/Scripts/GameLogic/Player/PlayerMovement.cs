7121169a9a1e8c7cee2ef5f142f2612438749fbb
josh <joshuapaullyle@gmail.com>
2020-10-05 16:44:31 -0400

    Movement scene

---
+++ Assets/Scripts/GameLogic/Player/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public ButtonClick upBtn;
+    public ButtonClick leftBtn;
+    public ButtonClick downBtn;
+    public ButtonClick rightBtn;
+
+    [SerializeField] private float speed = 0;
+    [SerializeField] private float dist = 0;
+
+    Vector3 pos;
+    Transform tr;
+ 
+    void Start()
+    {
+        pos = transform.position;
+        tr = transform;
+    }
+
+    void Update()
+    {
+        if (upBtn.buttonPressed && tr.position == pos)
+        {
+            pos += new Vector3(0, dist, 0);
+        }
+        else if (leftBtn.buttonPressed && tr.position == pos)
+        {
+            pos += new Vector3(-dist, 0, 0);
+        }
+        else if (downBtn.buttonPressed && tr.position == pos)
+        {
+            pos += new Vector3(0, -dist, 0);
+        }
+        else if (rightBtn.buttonPressed && tr.position == pos)
+        {
+            pos += new Vector3(dist,0,0);
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+    }
+}
---
+++ Assets/Scripts/UI/ButtonClick.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonClick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public bool buttonPressed;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        buttonPressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        buttonPressed = false;
+    }
+}
