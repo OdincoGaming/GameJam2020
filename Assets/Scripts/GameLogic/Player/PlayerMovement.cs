a7e4d7d9564cb809d1193370526953f96cfb9f4e
josh <joshuapaullyle@gmail.com>
2020-10-05 18:25:33 -0400

    Updated movement ui

--- Assets/Scripts/GameLogic/Player/PlayerMovement.cs
+++ Assets/Scripts/GameLogic/Player/PlayerMovement.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public ButtonClick upBtn;
+    public ButtonClick upLeftBtn;
+    public ButtonClick upRightBtn;
+
     public ButtonClick leftBtn;
-    public ButtonClick downBtn;
     public ButtonClick rightBtn;
+
+    public ButtonClick downBtn;
+    public ButtonClick downLeftBtn;
+    public ButtonClick downRightBtn;
 
     [SerializeField] private float speed = 0;
     [SerializeField] private float dist = 0;
@@ -21,23 +27,39 @@ public class PlayerMovement : MonoBehaviour
         tr = transform;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (upBtn.buttonPressed && tr.position == pos)
         {
-            pos += new Vector3(0, dist, 0);
+            pos += new Vector3(0,dist, 0);
+        }
+        else if (upLeftBtn.buttonPressed && tr.position == pos)
+        {
+            pos += new Vector3(-dist,dist, 0);
+        }
+        else if (upRightBtn.buttonPressed && tr.position == pos)
+        {
+            pos += new Vector3(dist,dist, 0);
         }
         else if (leftBtn.buttonPressed && tr.position == pos)
         {
-            pos += new Vector3(-dist, 0, 0);
+            pos += new Vector3(-dist,0, 0);
+        }
+        else if (rightBtn.buttonPressed && tr.position == pos)
+        {
+            pos += new Vector3(dist,0, 0);
         }
         else if (downBtn.buttonPressed && tr.position == pos)
         {
             pos += new Vector3(0, -dist, 0);
         }
-        else if (rightBtn.buttonPressed && tr.position == pos)
+        else if (downLeftBtn.buttonPressed && tr.position == pos)
         {
-            pos += new Vector3(dist,0,0);
+            pos += new Vector3(-dist, -dist, 0);
+        }
+        else if (downRightBtn.buttonPressed && tr.position == pos)
+        {
+            pos += new Vector3(dist, -dist, 0);
         }
 
         transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
